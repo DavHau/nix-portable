be19b6b35565b84c06d7cbdc3557368a1351870d
DavHau <hsngrmpf+github@gmail.com>
2022-01-24 18:01:49 +0700

    update proot to 5.3.0

--- proot/github.nix
+++ proot/github.nix
@@ -7,8 +7,8 @@ with builtins;
 
 pkgs.runCommand "proot-x86_46" {} ''
   bin=${builtins.fetchurl {
-    url = "https://github.com/proot-me/proot/releases/download/v5.2.0/proot-v5.2.0-x86_64-static";
-    sha256 = "1w729a5fz9wcxshn7vy4yg96qj59sxmd2by1gcl6nz57qjrl61pb";
+    url = "https://github.com/proot-me/proot/releases/download/v5.3.0/proot-v5.3.0-x86_64-static";
+    sha256 = "1nmllvdhlbdlgffq6x351p0zfgv202qfy8vhf26z0v8y435j1syi";
   }}
   mkdir -p $out/bin
   cp $bin $out/bin/proot
