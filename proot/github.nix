7dea678f962e1e975dd7af3fd88e4bb54e5652f3
DavHau <hsngrmpf+github@gmail.com>
2022-01-25 19:36:30 +0700

    downgrade proot to 5.2.0
    Didn't work on centos7 without PROOT_NO_SECCOMP

--- proot/github.nix
+++ proot/github.nix
@@ -5,10 +5,28 @@
 
 with builtins;
 
+let
+  version = "5.2.0";
+
+  systems = {
+    x86_64-linux = {
+      url = "https://github.com/proot-me/proot/releases/download/v${version}/proot-v${version}-x86_64-static";
+      sha256 = "1w729a5fz9wcxshn7vy4yg96qj59sxmd2by1gcl6nz57qjrl61pb";
+    };
+    aarch64-linux = {
+      url = "https://github.com/proot-me/proot/releases/download/v${version}/proot-v${version}-aarch64-static";
+      sha256 = "17ghp5n2jz38c4qk88yjc9cvdx9pcinmf2v7i7klnmzq5wzbkrzi";
+    };
+    armv7l-linux = {
+      url = "https://github.com/proot-me/proot/releases/download/v${version}/proot-v${version}-arm-static";
+      sha256 = "";
+    };
+  };
+in
+
 pkgs.runCommand "proot-x86_46" {} ''
   bin=${builtins.fetchurl {
-    url = "https://github.com/proot-me/proot/releases/download/v5.3.0/proot-v5.3.0-x86_64-static";
-    sha256 = "1nmllvdhlbdlgffq6x351p0zfgv202qfy8vhf26z0v8y435j1syi";
+    inherit (systems."${pkgs.buildPlatform.system}") url sha256;
   }}
   mkdir -p $out/bin
   cp $bin $out/bin/proot
